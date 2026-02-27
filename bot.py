01501234ec498bf3639c91bd21eb9f8784da0d40
whitemediacomua-bit <whitemedia.com.ua@gmail.com>
2026-02-27 12:14:01 +0200

    Enable dropping pending updates on polling

--- bot.py
+++ bot.py
@@ -36,4 +36,4 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("broadcast", broadcast))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-app.run_polling()
+app.run_polling(drop_pending_updates=True)
