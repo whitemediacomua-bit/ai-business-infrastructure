2a2a64b93f446f158f47031eac911af1026586c2
whitemediacomua-bit <whitemedia.com.ua@gmail.com>
2026-03-10 13:56:31 +0200

    Update ai.py

--- ai/ai.py
+++ ai/ai.py
@@ -4,25 +4,16 @@ from openai import OpenAI
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def ai_audit(text):
-
     prompt = f"""
-Ти маркетинговий експерт.
-
-Проаналізуй бізнес і дай:
-
-1. головну проблему
-2. 3 точки росту
-3. ідею реклами
-
-Бізнес:
-{text}
-"""
-
+    Ти маркетинговий експерт.
+    Проаналізуй бізнес і дай:
+    1. головну проблему
+    2. 3 точки росту
+    3. ідею реклами
+    Бізнес: {text}
+    """
     response = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
+        messages=[{"role": "user", "content": prompt}]
     )
-
     return response.choices[0].message.content
