73b88b6a6ddd226665f752d10c6829c285adb1d7
whitemediacomua-bit <whitemedia.com.ua@gmail.com>
2026-03-16 19:55:42 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,31 +3,30 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
-SessionLocal = sessionmaker(bind=engine)
 
 class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(Integer, unique=True, index=True)
-    username = Column(String, nullable=True)
+    __tablename__ = "users"    
+    id = Column(Integer, primary_key=True, index=True)    
+    telegram_id = Column(Integer, unique=True, index=True)    
+    username = Column(String, nullable=True)    
 
-def    create_table():
-    Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
-def    add_user(user_id, username):
-    session = SessionLocal()
-    user = session.query(User).filter(User.telegram_id == user_id).first()
-    if not user:
-        new_user = User(telegram_id=user_id, username=username)
-        session.add(new_user)
-        session.commit()
-    session.close()
+def add_user(user_id, username):
+    Session = sessionmaker(bind=engine)    
+    session = Session()    
+    user = session.query(User).filter(User.telegram_id == user_id).first()    
+    if not user:    
+        new_user = User(telegram_id=user_id, username=username)        
+        session.add(new_user)        
+        session.commit()        
+    session.close()    
 
-def    get_all_users():
-    session = SessionLocal()
-    users = session.query(User).all()
-    session.close()
-    return [(u.telegram_id, u.username) for u in users]
+def get_all_users():
+    Session = sessionmaker(bind=engine)    
+    session = Session()    
+    users = session.query(User).all()    
+    session.close()    
+    return [(u.telegram_id, u.username) for u in users]    
