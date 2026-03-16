f79ff2148bb6cb47447e45ebc64a18d270ded24d
whitemediacomua-bit <whitemedia.com.ua@gmail.com>
2026-03-16 17:28:05 +0200

    Update database.py

--- database.py
+++ database.py
@@ -12,10 +12,10 @@ class User(Base):
     __tablename__ = "users"  # ← ОБОВʼЯЗКОВО!
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, nullable=True)
-    def create_table():
+    def    create_table():
     Base.metadata.create_all(bind=engine)
 
-def add_user(user_id, username):
+def   add_user(user_id, username):
     session = SessionLocal()
     user = session.query(User).filter(User.id == user_id).first()
     if not user:
@@ -24,7 +24,7 @@ def add_user(user_id, username):
         session.commit()
     session.close()
 
-def get_all_users():
+def   get_all_users():
     session = SessionLocal()
     users = session.query(User).all()
     session.close()
