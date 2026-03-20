b7d0fb7bc7649d62d9591c36adc4f8e23f61c206
whitemediacomua-bit <whitemedia.com.ua@gmail.com>
2026-03-20 12:53:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 import os
-from dotenv import load_dotenv
 from ai.ai import ai_audit, ai_answer, ai_idea, ai_website, ai_hosting, ai_ads, ai_chatbot, ai_analytics, ai_mailing, ai_seo
 from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from database import create_table, add_user, add_request, get_all_users
 
-load_dotenv()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 DATABASE_URL = os.getenv(DATABASE_URL")
